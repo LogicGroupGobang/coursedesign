5d3bbfe2643549860af85c9c744a5fda2e8c8e44
root <root@DESKTOP-EIPEFPI.localdomain>
2019-01-07 15:32:10 +0800

    AI and reset completed

--- ipcore_dir/font_xmdf.tcl
+++ ipcore_dir/font_xmdf.tcl
@@ -155,10 +155,6 @@ utilities_xmdf::xmdfSetData $instance FileSet $fcount relative_path font.ngc
 utilities_xmdf::xmdfSetData $instance FileSet $fcount type ngc
 incr fcount
 
-utilities_xmdf::xmdfSetData $instance FileSet $fcount relative_path font.sym
-utilities_xmdf::xmdfSetData $instance FileSet $fcount type symbol
-incr fcount
-
 utilities_xmdf::xmdfSetData $instance FileSet $fcount relative_path font.v
 utilities_xmdf::xmdfSetData $instance FileSet $fcount type verilog
 incr fcount
